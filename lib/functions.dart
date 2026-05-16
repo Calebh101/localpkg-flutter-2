93d88f46a91e5c4172078bfcc10e3fdceba6f38f
caleb <calebh101dev@icloud.com>
2026-05-16 16:32:25 -0400

    Update localpkg and add FormKey typedef and FormKeyAddons on that

--- lib/functions.dart
+++ lib/functions.dart
@@ -247,3 +247,36 @@ extension NullIfEmptySelectableText on SelectableText {
   /// This returns null only if there are 0 characters. Spaces count.
   SelectableText? get nullIfEmpty => isEmpty ? null : this;
 }
+
+/// `GlobalKey<FormState>`
+typedef FormKey = GlobalKey<FormState>;
+
+/// Addons for [FormKey] (`GlobalKey<FormState>`).
+extension FormKeyAddons on FormKey {
+  /// This key's [currentState], with a null check.
+  ///
+  /// This key must be attached to a [Form] for this to not throw.
+  FormState get state => currentState!;
+
+  /// Validates every [FormField] that is a descendant of this [Form], and
+  /// returns true if there are no errors.
+  ///
+  /// The form will rebuild to report the results.
+  ///
+  /// See also:
+  ///  * [validateGranularly], which also validates descendant [FormField]s,
+  /// but instead returns a [Set] of fields with errors.
+  bool validate() => state.validate();
+
+  /// Validates every [FormField] that is a descendant of this [Form], and
+  /// returns a [Set] of [FormFieldState] of the invalid field(s) only, if any.
+  ///
+  /// This method can be useful to highlight field(s) with errors.
+  ///
+  /// The form will rebuild to report the results.
+  ///
+  /// See also:
+  ///  * [validate], which also validates descendant [FormField]s,
+  /// and return true if there are no errors.
+  Set<FormFieldState<Object?>> validateGranularly() => state.validateGranularly();
+}
